85d32dfc495de416f0e167e5f724e11bb08f3e1a
Gabriel Leandro <gabrielfarias2002@hotmail.com>
2024-01-12 10:52:37 -0300

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,7 +6,7 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot('mongodb+srv://gabrielleandro:99407685@cluster0-xvyyv.mongodb.net/nestjs-demo?retryWrites=true&w=majority')],
+  imports: [ProductsModule, MongooseModule.forRoot('MONGODBHOST')],
   controllers: [AppController],
   providers: [AppService],
 })
